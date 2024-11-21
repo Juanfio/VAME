678aad5bee2413f9c60d944e4f990d4211dfcab5
luiz <luiz_tauffer@hotmail.com>
2024-11-21 10:24:33 +0100

    tests

--- tests/test_initialize_project.py
+++ tests/test_initialize_project.py
@@ -23,21 +23,23 @@ def test_project_name_config(setup_project_not_aligned_data):
 
 
 def test_existing_project():
-    project = "test_existing_project"
+    project_name = "test_existing_project"
     videos = ["./tests/tests_project_sample_data/cropped_video.mp4"]
     poses_estimations = ["./tests/tests_project_sample_data/cropped_video.csv"]
     working_directory = "./tests"
 
     config_path_creation = init_new_project(
-        project_name=project,
+        project_name=project_name,
         videos=videos,
         poses_estimations=poses_estimations,
+        source_software="DeepLabCut",
         working_directory=working_directory,
     )
     config_path_duplicated = init_new_project(
-        project_name=project,
+        project_name=project_name,
         videos=videos,
         poses_estimations=poses_estimations,
+        source_software="DeepLabCut",
         working_directory=working_directory,
     )
     assert config_path_creation == config_path_duplicated
