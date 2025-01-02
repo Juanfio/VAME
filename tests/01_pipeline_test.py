e16913498d60b5385238157d7bd9c10fd9897f1e
luiz <luiz_tauffer@hotmail.com>
2025-01-02 11:38:12 +0100

    test pipeline vis

--- tests/01_pipeline_test.py
+++ tests/01_pipeline_test.py
@@ -1,9 +1,10 @@
 import xarray as xr
+from pathlib import Path
 
 
 def test_pipeline(setup_pipeline):
     pipeline = setup_pipeline["pipeline"]
-
+    project_path = pipeline.config["project_path"]
     sessions = pipeline.get_sessions()
     assert len(sessions) == 1
 
@@ -15,3 +16,12 @@ def test_pipeline(setup_pipeline):
         "orientation_reference_keypoint": "Tailroot",
     }
     pipeline.run_pipeline(preprocessing_kwargs=preprocessing_kwargs)
+
+    pipeline.visualize_preprocessing(
+        show_figure=False,
+        save_to_file=True,
+    )
+    save_fig_path_0 = Path(project_path) / "reports" / "figures" / f"{sessions[0]}_preprocessing_scatter.png"
+    save_fig_path_1 = Path(project_path) / "reports" / "figures" / f"{sessions[0]}_preprocessing_timeseries.png"
+    assert save_fig_path_0.exists()
+    assert save_fig_path_1.exists()
